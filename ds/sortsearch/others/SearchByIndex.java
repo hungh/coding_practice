a7cdf8c0b463ccc8906b2f715817cfca0be177f8
hungh <h.huynh@tmcbonds.com>
2017-01-03 17:00:00 -0500

    Search By Index

---
+++ ds/sortsearch/others/SearchByIndex.java
@@ -0,0 +1,15 @@
+/* (Skiena)
+4-33. Suppose that you are given a sorted sequence of distinct integers {a1,a2,…,an}.
+ Give an O(lgn) algorithm to determine whether there exists an i index such as ai=i.
+ For example, in {−10,−3,3,5,7}, a3=3. In {2,3,4,5,6,7}, there is no such i.
+*/
+
+package ds.sortsearch.others;
+
+public class SearchByIndex  {
+  public static void main(String[] args) {
+
+  }
+
+
+}
