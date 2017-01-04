de7adf25a66d0ad844cfde316fe9846ca783d98e
hungh <h.huynh@tmcbonds.com>
2017-01-04 16:56:29 -0500

    Sort a large text file

---
+++ ds/sortsearch/challenges/Problem4D37.java
@@ -0,0 +1,26 @@
+/*
+4-37. Implement versions of several different sorting algorithms,
+ such as selection sort, insertion sort, heapsort, mergesort, and quicksort.
+ Conduct experiments to assess the relative performance of these algorithms in 
+ a simple application that reads a large text file and reports exactly one instance of each word that 
+ appears within it. 
+ This application can be efficiently implemented by sorting all the words that occur in the text and then passing through 
+ the sorted sequence to identify one instance of each distinct word. Write a brief report with your conclusions.
+*/
+
+ // I only implement merge sort to sort a large text file 
+ // write it into a disk
+ // read the sorted file to scan for each instance of words in the file
+
+ // assume that we have limited memory size 
+ // text is from http://www.gutenberg.org/files/2600/2600-0.txt
+
+ package ds.sortsearch.challenges;
+
+ public class Problem4D37 {
+ 	public static void main(String[] args){
+
+ 	}
+
+
+ }
