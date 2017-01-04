728302485a1cfa59ecdb9c34bea3a91f81230fd4
hungdt1@yahoo.com <devuser@Hungs-MBP.home>
2017-01-03 21:13:52 -0500

    Search element value that has index value by logn

--- ds/sortsearch/others/SearchByIndex.java
+++ ds/sortsearch/others/SearchByIndex.java
@@ -6,9 +6,33 @@
 
 package ds.sortsearch.others;
 
+import ds.Common;
+
 public class SearchByIndex  {
   public static void main(String[] args) {
+  	int[] a = {-10, -3, 3, 5, 7};
+  	// int[] a = {2, 3, 4, 5, 6, 7};
+  	// int[] a = {1, 3, 5, 6};
+  	int r = getItemIndex(a);
+  	Common.log(r);
+  }
 
+
+  public static int getItemIndex(int[] a){
+  	int begin = 0, end = a.length - 1;
+  	int middle;
+  	while(begin <= end){
+
+  		middle = (begin + end)/2;
+  		if(middle + 1 < a[middle]){
+  			end = middle - 1;
+  		}else if(middle + 1 > a[middle]){
+  			begin = middle + 1;
+  		}else{
+  			return a[middle];
+  		}
+  	}
+  	return Integer.MIN_VALUE;
   }
 
 
