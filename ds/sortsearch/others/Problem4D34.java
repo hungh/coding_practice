38b6dd627c7665ebcf6fc2275328040a9a10875c
hungh <h.huynh@tmcbonds.com>
2017-01-04 10:46:35 -0500

    Skiena Problem 4-34: Find a smallest misssing number from sorted range logn

--- ds/sortsearch/others/Problem4D34.java
+++ ds/sortsearch/others/Problem4D34.java
@@ -7,21 +7,51 @@ is not present in a. For full credit, find the smallest such integer.
 
 package ds.sortsearch.others;
 
+import ds.Common;
+
 public class Problem4D34 {
 	public static void main(String[] args){
-		// n = 5, m = 12
-		int[] a = {1, 3, 8, 9, 12}; // returns 2
-		// int[] a = {1, 2, 3, 4, 6, 7, 8, 9, 12};
-		// will return 5
+		// int[] a = {1, 3, 8, 9, 12}; // returns 2
+		// int[] a = {1, 2, 4, 8, 22, 100}; // -> 3
+		// int[] a = {1, 2, 3, 4, 6, 7, 8, 9, 12}; // -> 5
+		int[] a = {1,  2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 19, 22, 25, 26, 27, 28, 30, 33, 36, 37, 44, 45, 88};
+		// int[] a = {1, 2, 3, 5, 100};
+		// int[] a = {1, 2, 99}; // -> 3	
+		int r = findSmallestM(a, 101);
+		Common.log("Result:" + r);
 
 	}
 
 
 	public static int findSmallestM(int[] a, int m){
+
+		if( a == null ) return Integer.MIN_VALUE; // NOT FOUND
+
 		int begin = 0, middle, end = a.length - 1;
-		while(begin <= end){
-			middle = (begin + end)/2;
-			
+		int endValue = -1;
+		int loop = 0;
+
+		if(a[begin] > 1) return 1;
+		if(a[end] < m) { endValue = a[end] + 1; };
+
+		while(begin < end){			
+			middle = (begin + end)/2; 
+
+			if(middle + 1 == a[middle]){
+				begin = middle + 1;
+
+			}else if (middle + 1 < a[middle]){
+				end = middle;
+			} 			
+			loop++;
 		}
+		// Common.log("begin=" + begin + ";end=" + end);
+		Common.log("#loop=" + loop + ";arr.length=" + a.length);
+
+		if(begin > 0 && begin < a.length) {
+			return a[begin - 1] + 1;
+		}
+		if(endValue > 0) return endValue;
+		return Integer.MIN_VALUE; // NOT FOUND
 	}
 }
