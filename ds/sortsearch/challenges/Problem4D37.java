86a341dfced3afe26cceeb24d0f33cdbae452338
hungh <h.huynh@tmcbonds.com>
2017-01-06 14:48:43 -0500

    Extracting words from a text

--- ds/sortsearch/challenges/Problem4D37.java
+++ ds/sortsearch/challenges/Problem4D37.java
@@ -24,12 +24,13 @@
  import ds.sortsearch.MergeSortS;
 
  public class Problem4D37 {
+
  	public static void main(String[] args){
  		// sort_large_text("http://www.gutenberg.org/files/2600/2600-0.txt");
- 		LinkListGR<String> h = toWords("This is a   \n\n\n  \n  test.   \n   Please do not take it seriously! Bye.  \n  \n\n\n");
+ 		LinkListGR<String> h = toWords("Hello Word. 123 You've completed.");
  		LinkListGR<String> curr = h;
  		while(curr != null){
- 			Common.log(curr.value);
+ 			System.out.print(curr.value + " ");
  			curr = curr.next;
  		}
  	}
@@ -71,35 +72,57 @@
  	public static LinkListGR<String> toWords(String s){
  		// can use String.split, but here we do a little different
  		if(s == null) return null;
- 		LinkListGR<String> head = new LinkListGR<String>("");
+ 		LinkListGR<String> head = new LinkListGR<String>(null); 		
  		LinkListGR<String> curr = head;
+ 		LinkListGR<String> prev = curr;
  		char sc; //  char
- 		int lastc = -1;; // last nonspace char
+ 		int lastc = -1;// last nonspace char
  		int space_idx = -1;
+ 		int lastInvalidIndex = -1;
+ 		String tmp;
  		StringBuilder mbuff = new StringBuilder();
- 		for(int i = 0; i < s.length(); i++) {
- 			// ignore special characters
- 			sc = s.charAt(i);
- 			
- 			// space 
- 			if(sc == ' ' || sc == '\n'){
+ 		for(int i = 0; i < s.length(); i++) { 			
+ 			sc = s.charAt(i); 			
+
+ 			if(sc == ' ' || sc == '\n' || sc == '\t'){
  				space_idx = i;
- 				if(lastc > 0) {
- 					String buffStr = mbuff.toString();
- 					// Common.log(mbuff);
- 					curr.value = buffStr; 
- 					curr.next = new LinkListGR<String>("");
+
+ 				if(lastInvalidIndex >= 0){
+ 					if(! isValidWord(mbuff, lastInvalidIndex)){
+						mbuff.setLength(0); 						
+ 						lastc = -1; 						
+ 					}
+ 					lastInvalidIndex = -1; 					
+ 				}
+ 				
+ 				if(lastc > 0) { 	 					
+ 					if(curr == null){
+ 						curr = new LinkListGR<String>(null); 		
+ 						prev.next = curr;
+ 					} 
+	 				curr.value = mbuff.toString();	
+ 					prev = curr;
  					curr = curr.next;
- 					// create a new buffer
  					mbuff = new StringBuilder();
  				}
- 				lastc = -1;
+ 				lastc = -1; 				
+
  			}else{
  				lastc  = i;
  				space_idx = -1; // no space yet
- 				mbuff.append(s.charAt(i));
+ 				char tc = s.charAt(i); 				
+ 				if( tc < 65 || tc > 122){
+ 					lastInvalidIndex = mbuff.length();
+ 				}
+ 				mbuff.append(tc);
  				if(i == s.length() - 1) { // last word
- 					curr.value = mbuff.toString();
+ 					if(isValidWord(mbuff, lastInvalidIndex)){
+ 						if(curr == null){
+ 							curr = new LinkListGR<String>(mbuff.toString()); 
+ 							prev.next = curr;
+ 						}
+ 					}
+ 					
  				}
  			}
 
@@ -108,5 +131,27 @@
  		return head;
  	}
 
+ 	private static boolean isValidWord(StringBuilder s, int lastInvalidIndex){
+ 		char tc;
+ 		if(lastInvalidIndex == 0){
+ 			tc = s.charAt(0);
+ 			if(tc != '"' && tc != '\'' ) return false;
+ 			s.deleteCharAt(0);
+ 		}
+ 		int len = s.length(); 		
+ 		tc = s.charAt(len - 1);
+ 		if(lastInvalidIndex == len - 1){
+ 			if ( tc != '!' && tc != '?' && tc != '.' && tc != ')' && tc != ';' 
+ 				&& tc != ',' && tc == '\'' && tc == '"') return false;
+ 			s.deleteCharAt(lastInvalidIndex);
+ 			lastInvalidIndex--;
+ 		}
+ 		
+ 		for(int i = lastInvalidIndex; i >= 0; i--){
+ 			tc = s.charAt(i); 			
+ 			if(tc != '-' && tc != '\'' && (tc < 65 || tc > 122)) return false;
+ 		}
+ 		return true;
+ 	}
 
  } 		
