4bbb78941cbf3451d1c039e81ce6938a5b153f20
hungdt1@yahoo.com <hungdt1@yahoo.com>
2017-01-05 21:43:05 -0500

    Merge Sort Large Text - added split string into Linked Lists

--- ds/sortsearch/challenges/Problem4D37.java
+++ ds/sortsearch/challenges/Problem4D37.java
@@ -17,10 +17,96 @@
 
  package ds.sortsearch.challenges;
 
+ import java.net.*;
+ import java.io.*;
+ import ds.Common;
+ import ds.list.LinkListGR;
+ import ds.sortsearch.MergeSortS;
+
  public class Problem4D37 {
  	public static void main(String[] args){
+ 		// sort_large_text("http://www.gutenberg.org/files/2600/2600-0.txt");
+ 		LinkListGR<String> h = toWords("This is a   \n\n\n  \n  test.   \n   Please do not take it seriously! Bye.  \n  \n\n\n");
+ 		LinkListGR<String> curr = h;
+ 		while(curr != null){
+ 			Common.log(curr.value);
+ 			curr = curr.next;
+ 		}
+ 	}
 
+ 	/*
+ 	read a large text file from url, write it down into 
+ 	small chunks and merge sorted chunks into one.
+ 	*/
+ 	public static boolean sort_large_text(String url_str){
+ 		URLConnection conn = null;
+ 		BufferedReader buff = null;
+ 		BufferedWriter out = null;
+ 		try{
+ 			URL url = new URL (url_str);
+ 			conn = url.openConnection();
+			buff = new BufferedReader (new InputStreamReader( conn.getInputStream()));	
+
+			out = new BufferedWriter(new FileWriter("out.txt"));
+			String line;
+			while( (line = buff.readLine()) != null) {
+				out.write(line + " ");
+			}
+ 		}catch(Exception e) {
+ 			e.printStackTrace();
+ 		}finally {
+ 			try{
+ 				if(out != null) out.close();
+ 				if(buff != null) buff.close();	
+ 			}catch(Exception e1){
+ 				e1.printStackTrace();
+ 			}
+ 			
+ 		} 
+ 		return true;
+ 			
+ 	}
+
+ 	// parse a string into words
+ 	public static LinkListGR<String> toWords(String s){
+ 		// can use String.split, but here we do a little different
+ 		if(s == null) return null;
+ 		LinkListGR<String> head = new LinkListGR<String>("");
+ 		LinkListGR<String> curr = head;
+ 		char sc; //  char
+ 		int lastc = -1;; // last nonspace char
+ 		int space_idx = -1;
+ 		StringBuilder mbuff = new StringBuilder();
+ 		for(int i = 0; i < s.length(); i++) {
+ 			// ignore special characters
+ 			sc = s.charAt(i);
+ 			
+ 			// space 
+ 			if(sc == ' ' || sc == '\n'){
+ 				space_idx = i;
+ 				if(lastc > 0) {
+ 					String buffStr = mbuff.toString();
+ 					// Common.log(mbuff);
+ 					curr.value = buffStr; 
+ 					curr.next = new LinkListGR<String>("");
+ 					curr = curr.next;
+ 					// create a new buffer
+ 					mbuff = new StringBuilder();
+ 				}
+ 				lastc = -1;
+ 			}else{
+ 				lastc  = i;
+ 				space_idx = -1; // no space yet
+ 				mbuff.append(s.charAt(i));
+ 				if(i == s.length() - 1) { // last word
+ 					curr.value = mbuff.toString();
+ 				}
+ 			}
+
+ 		}
+ 		
+ 		return head;
  	}
 
 
- }
+ } 		
