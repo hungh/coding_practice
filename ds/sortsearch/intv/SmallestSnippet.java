b40cc82dc0529a100a767bd12d9e1b5eb5658929
hungdt1@yahoo.com <hungdt1@yahoo.com>
2017-01-11 22:11:49 -0500

    Find smallest snippet of words

---
+++ ds/sortsearch/intv/SmallestSnippet.java
@@ -0,0 +1,19 @@
+/*
+4-45. Given a search string of three words, 
+find the smallest snippet of the document that contains all three of the search words---i.e., 
+the snippet with smallest number of words in it. You are given the index positions 
+where these words occur in the document, such as word1: (1, 4, 5), word2: (3, 9, 10), and word3: (2, 6, 15).
+ Each of the lists are in sorted order, as above.
+*/
+package ds.sortsearch.intv;
+
+public class SmallestSnippet {
+	public static void main(String[] args){
+		// w1: 1, w2: 2, w3: 3
+		// 0 w1 5 w3 w2 4 w1 w3 8 w3 w2 w1 0 w3 w2 
+	}
+
+	public static String findSnippet(String[] s, int[] a, int[] b, int[] c){
+
+	}
+}
