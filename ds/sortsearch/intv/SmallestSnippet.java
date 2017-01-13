2cd841e7023d4ab3502956a9a675a7449e8b5a9e
hungcs@gmail.com <h.huynh@tmcbonds.com>
2017-01-13 09:47:29 -0500

    Smallest Snippet with indexed words

--- ds/sortsearch/intv/SmallestSnippet.java
+++ ds/sortsearch/intv/SmallestSnippet.java
@@ -8,58 +8,79 @@ where these words occur in the document, such as word1: (1, 4, 5), word2: (3, 9,
 package ds.sortsearch.intv;
 
 import ds.Common;
+import ds.Pair;
 
 public class SmallestSnippet {
 	public static void main(String[] args){
-		// w1: 1, w2: 2, w3: 3
-		// 0 w1 5 w3 w2 4 w1 w3 8 w3 w2 w1 0 w3 w2 
-
-		 //w1: [1, 6, 9]  w2: [4, 8, 12]    w3: [3, 7, 11]
-
-		 //1 2 3 4  6 7 11  8
-		 int[] a = {1, 3 , 6, 7, 11, 15, 19, 22, 30};
-		 int r = findClosestGap(a, Integer.parseInt(args[0]));
-		 Common.log(r);
+		int[] a = new int[] {1, 6, 11};
+		int[] b = new int[] {4, 10, 14};
+		int[] c = new int[] {3, 7, 9, 13};
+		String result = findSnippet(new String[] {"0", "w1", "5", "w3", "w2", "4", "w1", "w3", "8", "w3", "w2", "w1", "0","w3","w2" }, a, b, c);
+		Common.log(result);
 	}
-
 	/*
 	Analysys:
 	scan w1:
 	for each w1: do a binary Search on  w2 and w3 for the closest element to w1
 	keep track of the min snippet length
+	Time complexity: O(n* 2logn) ~ O(nlogn)
 	*/
-
 	public static String findSnippet(String[] s, int[] a, int[] b, int[] c){
-		int i = 0;
-		for(; i < a.length; i++){
-
+		int clsB, clsC, t;
+		Pair minMax, min = new Pair(0, Integer.MAX_VALUE);
+		for(int i = 0; i < a.length; i++){
+			clsB = findClosestGap(b, a[i]);
+			clsC = findClosestGap(c, a[i]);
+			minMax = getMinMax(a[i], clsB, clsC);
+			t = minMax.b - minMax.a;
+			if(min.b - min.a > t) min = minMax;
 		}
-		return null;
+		Common.log("min" + min);
+		return getString(s, min.a, min.b);
 	}
 	// where a is a sorted array of integers
 	public static int findClosestGap(int[] a, int x){
 		if(a.length == 1) return a[0];
-
 		int s = 0, e = a.length - 1, middle;
-		int min = Integer.MAX_VALUE;
-		while(s <= e){
+		while(s < e){
 			middle = (s + e)/2;
-			Common.log("amiddle=" + a[middle]);
 			if(x  > a[middle]){
 				s = middle + 1;
 			}else if(x < a[middle]){
 				e = middle - 1;
-			} else {
-				// will not happen
-				throw new IllegalArgumentException("Index clash exception");
-			}
+			} else {				
+				return a[middle];
+			}			
 		}
-
 		if(s == 0){
-			if(Math.abs(x - s[0]) > Math.abs(x - s[1])) return 1; else return 0;
+			if(Math.abs(x - a[0]) > Math.abs(x - a[1])) return a[1]; else return a[0];
 		} else if( s == a.length - 1){
-
+			if(Math.abs(x - a[a.length - 1]) < Math.abs(x - a[a.length - 2])) return a[a.length - 1]; else return a[a.length - 2];
+		} else {
+			Pair[] ts = new Pair[3];
+			ts[0] = new Pair(s, Math.abs(x - a[s]));
+			ts[1] = new Pair(s - 1, Math.abs(x - a[s  - 1]));
+			ts[2] = new Pair(s + 1, Math.abs(x - a[s  + 1]));
+			Pair min = ts[0];
+			for(int i = 1; i < 3; i++) if(min.b  > ts[i].b) min = ts[i];
+			return a[min.a];
+		}		
+	}
+	// default to min if 'isMax = false'
+	private static Pair getMinMax(int... a) {		
+		int min = a[0], max = min;
+		Pair r = new Pair(min, max);
+		for(int i = 1; i < 3; i++){
+			if(max < a[i]) max = a[i];
+			if(min > a[i]) min = a[i];
 		}
-		return a[s];
+		r.a = min; 
+		r.b = max;
+		return r;
+	}
+	private static String getString(String[] s, int min, int max){
+		StringBuilder buff = new StringBuilder();
+		for(int i = min; i <= max; i++) buff.append(s[i]).append(' ');
+		return buff.toString();
 	}
 }
