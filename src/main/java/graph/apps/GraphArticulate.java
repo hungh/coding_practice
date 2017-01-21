6e96c060089dfee2310a1378f67d38d2840b642b
hungdt1@yahoo.com <hungdt1@yahoo.com>
2017-01-21 11:09:51 -0500

    Graph Articulation vertex

--- src/main/java/graph/apps/GraphArticulate.java
+++ src/main/java/graph/apps/GraphArticulate.java
@@ -7,6 +7,9 @@ package graph.apps;
 import graph.common.GraphAL;
 import ds.Common;
 
+/*
+Skiena Algoritm Design and Manual (C -> Java)
+*/
 public class GraphArticulate extends GraphAL {
 
 	public static final int TREE = 0;
@@ -48,6 +51,40 @@ public class GraphArticulate extends GraphAL {
 
 	@Override
 	public void process_vertex_late(int v){
+		boolean root; // is vertex root of the dfs tree
+		int time_v; //earliest reachable time for v
+		int time_parent; // earliest reachable time for parent's v
+
+		// root does not have parent
+		if(parent[v] < 1) {
+			if(tree_out_degree[v] > 1) // if it has more than 1 branch, then it is an articulation vertex
+				Common.log("Root is an articulation vertex " + v);
+			return; // nothing to do
+		}
+		// is v's parent root?
+		root = parent [parent[v]] < 1;
+		// v's parent is also its reachable ancester but this parent is not a root
+		if(reachable_ancestor[v] == parent[v] && !root){
+			Common.log("v's parent is an articulate vertex v=" + v + ";parent=" + parent[v]);
+			
+		}
+		// just discovered v (after the second call of process_vertex_early, v's parent and now v) [v is not processed yet]
+		if(reachable_ancestor[v] == v){
+			// v's parent must be an articulation vertex
+			Common.log("Bridge articulation vertex " +  parent[v]);
+			// if v is not a leaf then it is an articulate vertex
+			if(tree_out_degree[v] > 0) // a leaf must not have any branch-out
+				Common.log("v is an bridge articulate vertex " + v);
+		}
+		time_v = entry_time [reachable_ancestor[v]];
+		time_parent  = entry_time [reachable_ancestor[parent[v]]];
+
+		// entry of time v's reachable ancester is earlier (higher) than its parents'
+		// we have to update parent of v reachable ancester to to higher one, which is v
+		// ex, ->(1) -> (2) -> (3) -> (v) and (1) -> (v), update reachable_ancester[3] -> 1
+		if(time_v < time_parent){
+			reachable_ancestor[parent[v]] = reachable_ancestor[v];
+		}
 
 	}
 
