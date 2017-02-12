86545bb1ac2889a4f077f94fdfe4cc1760953f61
hungdt1@yahoo.com <hungdt1@yahoo.com>
2017-02-12 15:48:53 -0500

    War Story: The dual graph  of a triangulation

--- src/main/java/graph/common/EdgeNode.java
+++ src/main/java/graph/common/EdgeNode.java
@@ -1,6 +1,8 @@
 package graph.common;
 
 public class EdgeNode {
+	public EdgeNode(){}
+	public EdgeNode(int y) { this.y = y; }
 	public int y;  // adjancency info
 	public Integer weight;
 	public EdgeNode next;
--- src/main/java/graph/triangula/TriangulateGraph.java
+++ src/main/java/graph/triangula/TriangulateGraph.java
@@ -8,19 +8,25 @@ import java.util.*;
 
 import ds.Common;
 import graph.common.Triangle;
+import graph.common.EdgeNode;
 import graph.common.Graph;
 import graph.common.GraphAL;
 
 public class TriangulateGraph extends GraphAL {
 	public static final int MAXV = Graph.MAXV;
 
-	protected Triangle[] triangles = new Triangle[MAXV];
+	protected Triangle[] triangles = new Triangle[MAXV + 1]; // index is a vertex
 	protected int numOfTriangles;
+
+	// the second graph (where each vertex is a triangle) using Adjacency List
+	protected EdgeNode[] t2Edges = new EdgeNode[MAXV + 1];
 
 	public static void main(String[] args){
 		TriangulateGraph g = new TriangulateGraph(false);
-		g.read_graph("/triangleGraph.txt");
+		g.read_graph("/triangleGraph.txt"); // See Figure 5.8 (Skiena book)
 		g.printAdjTriangle();
+		Common.log(" **** The dual graph (dashed lines) of a triangulation : (Figure 5.8)");
+		g.print_graph();
 	}
 
 	public TriangulateGraph(boolean _directed){
@@ -32,22 +38,103 @@ public class TriangulateGraph extends GraphAL {
 		scanInput(sc, false);
 
 		// Read triangles
-		int t, x, y, z;
+		int t;
+		int[] x = new int[3];
+
 		numOfTriangles = sc.nextInt();
 		Common.log("numOfTriangles=" + numOfTriangles);
 
 		for(int i = 1; i <= numOfTriangles; i++) {
-			t = sc.nextInt();
-			x = sc.nextInt();
-			y = sc.nextInt();
-			z = sc.nextInt();	
+			t = sc.nextInt(); // triangle number 
+			x[0] = sc.nextInt(); // vertex number
+			x[1] = sc.nextInt();
+			x[2] = sc.nextInt();
 
-			// Common.log("t =" + t + ";x=" + x + ";y=" + y + ";z=" + z);
-			updateAdjTriangle(t, x);	
-			updateAdjTriangle(t, y);
-			updateAdjTriangle(t, z);
+			int adjSize = 0;
+			// find the commons between 3 lists
+			// x0 and x1
+			int commonVertex = findDup(triangles [x[0]], triangles [x[1]], t);
+			
+			if(commonVertex > 0){
+				adjSize = insertNewAdj(t, commonVertex);
+			}
+
+			if(adjSize < 3){
+				// x1 and x2
+				commonVertex = findDup(triangles [x[1]], triangles [x[2]], t);	
+				if(commonVertex > 0) adjSize = insertNewAdj(t, commonVertex);
+			}
+			
+			if(adjSize < 3){
+				// x0 and x2
+				commonVertex = findDup(triangles [x[0]], triangles [x[2]], t);
+				if(commonVertex > 0) adjSize = insertNewAdj(t, commonVertex);
+			}
+
+			// update triangle adjacency list
+			boolean found;
+			for(int j = 0; j < 3; j++){
+				if(triangles[x[j]] == null) {
+					// insert new 
+					triangles[x[j]] = new Triangle(t);
+				}else {
+					Triangle temp = triangles[x[j]];
+					found = false;
+					while(temp != null){
+						if(temp.t == t) { found = true; break;}
+						temp = temp.next;
+					}
+					if(!found) {
+						// insert at head
+						temp = new Triangle(t);
+						temp.next = triangles[x[j]];
+						triangles[x[j]] = temp;
+					}
+
+				}	
+			}
 		}
 		sc.close();		
+	}
+
+	// returns the number of adj element (Max : 3), t is the new vertex number
+	public int insertNewAdj(int t, int adjValue){
+		EdgeNode temp = t2Edges[t];
+		int size = 0;
+		if(temp == null) t2Edges[t] = new EdgeNode(adjValue);
+		else {
+			boolean found = false;
+			while(temp != null){
+				if(temp.y == adjValue) found = true;
+				temp = temp.next;
+				size++;
+			}
+			if(!found){
+				// insert at head
+				temp = new EdgeNode(adjValue);
+				temp.next = t2Edges[t];
+				t2Edges[t] = temp;
+			}
+		}
+		return size;
+
+	}
+
+	// find the the first duplicate between t1 and t2, but ignore any value with 'ignoreT'
+	public int findDup(Triangle t1, Triangle t2, int ignoreT){
+		if(t1  == null || t2 == null) return -1;
+		Map<Integer, Object> dMap = new HashMap<Integer, Object>();
+		Triangle temp = t1;
+		while(temp != null) {
+			dMap.put(temp.t, null);
+			temp = temp.next;
+		}
+		temp = t2;
+		while(temp != null){
+			if(dMap.containsKey(temp.t) && temp.t != ignoreT) return temp.t;
+			temp = temp.next;
+		} 
+		return -1; // no dup found
 	}
 
 	@Override
@@ -55,11 +142,25 @@ public class TriangulateGraph extends GraphAL {
 		// don't close the file yet, close it manually
 	}
 
+	@Override
+	public void print_graph(){
+		EdgeNode p;
+		for(int i = 1; i <= numOfTriangles; i++){
+			Common._log(i + " :");
+			p = t2Edges[i];
+			while(p != null){
+				Common._log(" " + p.y);
+				p = p.next;
+			}
+			Common.log("");
+		}
+	}
+
 	public void printAdjTriangle(){
 		Triangle tri;
 		for(int i = 1; i <= nvertices; i++){
 			tri = triangles[i];
-			Common.log("Vertex #"+ i);
+			Common._log("Vertex: "+ i + ": ");
 			while(tri != null) {
 				Common._log(" " + tri.t);
 				tri = tri.next;
@@ -67,12 +168,5 @@ public class TriangulateGraph extends GraphAL {
 			Common.log("");
 		}
 	}
-
-	protected void updateAdjTriangle(int triangleNumber, int vertexNumber){
-		Triangle triangle = new Triangle(triangleNumber);
-		triangle.next = triangles[vertexNumber];
-		triangles[vertexNumber] = triangle;	
-	}
-
 
 }
