fd6f07dab32c958ae7d225f2785d4559c01264a0
Hung Huynh <hungcs@gmail.com>
2017-02-23 16:54:48 -0500

    Problem 5-19

---
+++ src/main/java/graph/analysis/Problem5_19.java
@@ -0,0 +1,24 @@
+/*
+5-19. [5] The diameter of a tree T = (V,E) is given by
+
+  max δ(u, v)
+u,v∈V
+
+(where δ(u, v) is the number of edges on the path from u to v). Describe an efficient
+algorithm to compute the diameter of a tree, and show the correctness and analyze
+the running time of your algorithm.
+*/
+
+package graph.analysis;
+
+import ds.Common;
+import graph.common.*;
+
+public class Problem5_19 extends GraphAL {
+	public static void main(String[] args){}
+
+	public Problem5_19 (boolean directed) {
+		super(directed);
+	}
+
+}
