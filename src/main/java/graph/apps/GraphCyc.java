bcb9c0dd35aacf8cf66b06d0eee4c5fff90db2e0
Hung Huynh <hungdt1@yahoo.com>
2017-02-28 21:34:42 -0500

    Solving problem 5-23 in Algo book

--- src/main/java/graph/apps/GraphCyc.java
+++ src/main/java/graph/apps/GraphCyc.java
@@ -11,11 +11,13 @@ import ds.Common;
 Find cycles in a directed graph
 */
 public class GraphCyc extends GraphAL {
+	private int starting;
 	public static void main(String[] args){
-		GraphCyc graph = new GraphCyc(true);
-		graph.read_graph("/graph2.txt");
-		graph.reset();
-		graph.dfs(1);
+		GraphCyc g = new GraphCyc(true);
+		g.read_graph("/graph2.txt"); 
+		g.reset();
+		g.setStartingVertex(4);
+		g.dfs(g.getStartingVertex());
 	}
 
 	public GraphCyc(boolean directed){
@@ -25,12 +27,20 @@ public class GraphCyc extends GraphAL {
 	@Override
 	public void process_edge(int x, int y){
 		Common.log("\tprocessing edge (" + x + "," + y + ")");	
-		if( (parent[y] != -1 || y == 1) && (parent[y] != x) ) { // y=1, the root node, does not have any children
+		if( (parent[y] != -1 || y == starting) && (parent[y] != x) ) { // y=starting, the root node, does not have any children
 			Common.log("parent[ " + x + "]=" + parent[y]);
 			Common.log("Found circle (" + x + "," + y + ")");
 			find_path(y, x);
 			finished = true;
 		}
+	}
+
+	public int getStartingVertex() {
+		return starting;
+	}
+
+	public void setStartingVertex(int starting){
+		this.starting = starting;
 	}
 
 }
--- src/main/java/graph/directed/Problem5_23.java
+++ src/main/java/graph/directed/Problem5_23.java
@@ -16,13 +16,52 @@ at j.
 
 package graph.directed;
 
+import java.util.*;
 import ds.Common;
 import graph.common.*;
 
 public class Problem5_23 extends GraphAL {
-	public static void main(String[] args){}
+
+	public static void main(String[] args){
+		Problem5_23 g= new Problem5_23(true);
+		g.read_graph("/problem5_23_2.txt"); //problem5_23.txt
+		g.print_graph();
+		int max = g.findMinRows(3, 0) + 1;
+		Common.log("Max rows=" + max);
+	}
 
 	public Problem5_23 (boolean directed) {
 		super(directed);
+	}
+
+	//(a)
+	// to find if the graph has a circle or not, see graph.apps.GraphCyc
+
+	//(b)
+	public int findMinRows(int s, int row) {
+		int r = row;
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+
+		queue.push(s);
+		EdgeNode p;
+		int begin;
+		while(! queue.isEmpty()) {
+			begin = queue.pop();
+			p  = edges[begin];
+			while(p != null) {
+				if(! discovered[p.y]){
+					if(begin < p.y) {
+						Common.log("\t\t Adding to group [" + row + "] vertex " + p.y);
+						queue.push (p.y);
+						discovered[p.y] = true;
+					}else {
+						r = findMinRows(p.y, row + 1);
+					}	
+				}
+				p = p.next;
+			}
+			discovered[begin] = true;
+		}
+		return Math.max(row, r);
 	}
 }
