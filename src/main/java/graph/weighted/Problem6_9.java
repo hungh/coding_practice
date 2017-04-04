5161dd01b4b63025893f843ff24f17f0ab66b098
hungdt1@yahoo.com <hungdt1@yahoo.com>
2017-04-03 21:56:26 -0400

    Minimum connected weighted subset T of graph G

--- src/main/java/graph/weighted/Problem6_9.java
+++ src/main/java/graph/weighted/Problem6_9.java
@@ -14,16 +14,18 @@ import java.util.*;
 import ds.Common;
 
 import graph.common.*;
+import graph.common.weighted.*;
+
 
 /*
-Answeer to (a):
+Answer to (a):
 ---------------
 This is a problem of finding a subset T of edges from G and T is connected.
 Therefore, if an edge is negative, it will be included into T.
 Clearly, T does not have to be an MST. 
 Note that, if all edges have positive weights, then T is an MST.
 
-Answeer to (b):
+Answer to (b):
 ---------------
 This is similar to Prims or Kruskals but it has a subtle modification is that:
 Let say, we are going to use Kruskals as a base line, then
@@ -32,6 +34,72 @@ When considering an edge:
   then it is added.
 
 */
-public class Problem6_9 {
+public class Problem6_9 extends KruskalsMST {
+	public static void main(String[] args){
+		Problem6_9 g = new Problem6_9();
+		g.read_graph("/figure6_3.txt");
+		g.print_graph();
+		g.getMinWeightSetT();
 
+	}
+	public Problem6_9() {
+		super(true, false);
+	}
+
+	public GraphAL getMinWeightSetT() {
+		int min_sum = 0;
+
+		SetUnion s = new SetUnion();
+		init_set_union(s, nvertices);
+		e = to_edge_array();		
+		Collections.sort(e);
+		
+		GraphAL minT = GraphUtils.getNewEmptyGraph(nvertices, true, false);
+		int direction;
+		for(EdgePair ep: e){
+			if(min_sum + ep.w < min_sum){
+				minT.insert_edge (ep.y, ep.x, ep.w, directed);
+				min_sum += ep.w;
+			} else if(! SetUnion.same_component(s, ep.x, ep.y)){
+				direction = SetUnion.union_sets(s, ep.x, ep.y);
+				min_sum += ep.w;
+				if(direction > 0) 
+					minT.insert_edge (ep.x, ep.y, ep.w, directed);
+				else if (direction < 0)
+					minT.insert_edge (ep.y, ep.x, ep.w, directed);
+			}
+		}
+
+		return minT;
+	}
+
+	@Override
+	public List<EdgePair> to_edge_array(){
+		EdgeNode p;
+		List<EdgePair> pairs = new ArrayList<EdgePair>();
+		Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
+		Integer r;
+		for(int i = 1; i <= nvertices; i++) {
+			p = edges[i];
+			while(p != null){
+				r = cache.get(p.y);
+				if(r == null) {
+					cache.put(p.y, i);
+				} else if (r == i){
+					continue;
+				}
+				pairs.add(new EdgePair(i, p.y, p.weight));
+				p = p.next;
+			}
+		}
+		return pairs;
+	}
 }
+
+
+
+
+
+
+
+
