7c286e1415aec62127c315dbdfc6f017ec24c3e9
Hung Huynh <hungcs@gmail.com>
2017-04-05 16:56:21 -0400

    Added hashCode and equals into EdgeNode

--- src/main/java/graph/common/EdgeNode.java
+++ src/main/java/graph/common/EdgeNode.java
@@ -14,6 +14,20 @@ public class EdgeNode implements Comparable <EdgeNode>{
 	}
 
 	@Override
+	public boolean equals(Object o){
+		if(o instanceof EdgeNode) {
+			EdgeNode obj = (EdgeNode)o;
+			return (y == obj.y) && (weight == obj.weight);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return new StringBuilder().append(y).append(weight).hashCode();
+	}
+
+	@Override
 	public int compareTo(EdgeNode o){
 		return weight - o.weight;
 	}
