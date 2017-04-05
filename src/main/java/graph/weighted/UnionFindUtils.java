713d209eefc2889ba1c0511381da04a0afa580e4
Hung Huynh <hungcs@gmail.com>
2017-04-05 16:37:09 -0400

    Union Find Utilities

---
+++ src/main/java/graph/weighted/UnionFindUtils.java
@@ -0,0 +1,28 @@
+/*
+6-12. [5] Devise an efficient data structure to handle the following operations on a
+weighted directed graph:
+(a) Merge two given components.
+(b) Locate which component contains a given vertex v.
+(c) Retrieve a minimum edge from a given component
+*/
+
+package graph.weighted;
+
+import java.util.*;
+import graph.common.weighted.*;
+
+
+public class UnionFindUtils   {
+
+	public SetUnion merge(SetUnion s1, SetUnion s2){
+		return null;
+	}
+
+	public boolean contains(SetUnion s, int x){
+		return false;
+	}
+
+	public EdgePair getMinEdge(SetUnion s){
+		return null;
+	}
+}
