6141e32c153f11b45d9a1e8c9d8b1c6e0b04d6dc
Hung Huynh <hungcs@gmail.com>
2017-04-11 10:58:47 -0400

    Solved problem 6.19: find a minimum weighted cycle in a graph

--- src/main/java/graph/weighted/Problem6_19.java
+++ src/main/java/graph/weighted/Problem6_19.java
@@ -18,7 +18,7 @@ public class Problem6_19 extends Floyd {
 	public static void main(String[] args) {
 		Problem6_19 g = new Problem6_19(true, true);
 		g.setDiameter(20);
-		g.read_graph("/problem6_19.txt"); // Reused prims' See Figure 6.3 on Page 196.
+		g.read_graph("/problem6_19.2.txt"); // Reused prims' See Figure 6.3 on Page 196.
 		g.print_graph_pretty(g.getMatrix());		
 		g.floyd();
 		Common.log("Calculating shortest paths matrix ...");
@@ -28,20 +28,68 @@ public class Problem6_19 extends Floyd {
 	public Problem6_19(boolean weighted, boolean directed) {
 		super(weighted, directed);
 	}
-
-	public void floyd(){
+	// O(n^3)
+	public void floyd(){		
 		this.cloneWeight();
 
 		int i, j; // dimension counters
 		int k;    // intermediate vertex counter
-		int through_k; // distance through vertex k
+		int through_k; // distance through vertex k		
+		int d = getDiameter()  - 1;
+		int minCycWeight = d;
+
+		// to find minimum cycle path
+		int start = -1, middle = start;
+
+		for(i = 1; i <= nvertices; i++) parent[i] = -1;
 
 		for(k = 1; k <= nvertices; k++)
 			for(i = 1; i <= nvertices; i++)
 				for(j = 1; j <= nvertices; j++){
+
+					// update parent if it is not assigned yet and has to be neighbor
+					if(m[i][k] < d){
+						// the new parent comes from a smaller edge
+						if( (parent[k] < 0) || (m[i][k] < m[parent[k]][k]) )
+							parent[k] = i;						
+					}
+
+					if(m[k][j] < d) {
+						// the new parent comes from a smaller edge
+						if((parent[j] < 0) || (m[k][j] < m[parent[j]][j]))
+							parent[j] = k;						
+					}
+
 					through_k = weight[i][k] + weight[k][j];
-					if(through_k < weight[i][j]) weight[i][j] = through_k;
-					if(i == j) Common.log("cycle [ " + i + " (" + k + ") " + j + "]= " + weight[i][j]); // k has to be neighboring to j
+
+					if(through_k < weight[i][j]) {
+						weight[i][j] = through_k;																	
+					}
+
+					// k has to be neighboring to j	
+					if( (i == j) && (weight[i][j] < d) && (m[k][j] < d)){
+						// Common.log("cycle [ " + i + " (" + k + ") " + j + "]= " + weight[i][j]+ "; " + m[k][j]); 
+
+						if(minCycWeight > weight[i][j]) {
+							minCycWeight = weight[i][j];
+							start = i;
+							middle = k;
+							// re-update parent when seeing a min cycle
+							parent[j] = k;
+						}						
+					} 
 				}
+
+		if(start > 0){
+			Common.log("Min Cycle Weight: " + minCycWeight);
+			int v = middle;
+			Common._log("(*) Min Cycle Path: " + middle);
+			while(parent[v] != middle){
+				Common._log(" - " + parent[v]);
+				v = parent[v];
+			}
+			Common.log(" - " + middle + "\n");
+		}
+
 	}
 }
